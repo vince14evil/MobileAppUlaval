5ca9be9097517dfa7e616d770c659dfd35ee58af
Vincent bernier <vince14_evil@hotmail.com>
2016-04-28 18:46:08 -0400

    Add toolbar

--- app/src/main/java/barchallenges/ima/ulaval/ca/barchallenge/MainActivity.java
+++ app/src/main/java/barchallenges/ima/ulaval/ca/barchallenge/MainActivity.java
@@ -1,8 +1,9 @@
 package barchallenges.ima.ulaval.ca.barchallenge;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbarTop = (Toolbar) findViewById(R.id.toolbar_bottom);
+        setSupportActionBar(toolbarTop);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         Button btnPrize = (Button)findViewById(R.id.buttonPrize);
         btnPrize.setOnClickListener(new View.OnClickListener() {
