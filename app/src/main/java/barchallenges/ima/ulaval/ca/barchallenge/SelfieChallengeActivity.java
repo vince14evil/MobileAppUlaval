9ac78b51502206440ff066d495a51e85650f69cd
Vincent bernier <vince14_evil@hotmail.com>
2016-04-28 17:05:05 -0400

    Fix an issue with the slefie challenge

--- app/src/main/java/barchallenges/ima/ulaval/ca/barchallenge/SelfieChallengeActivity.java
+++ app/src/main/java/barchallenges/ima/ulaval/ca/barchallenge/SelfieChallengeActivity.java
@@ -1,23 +1,20 @@
 package barchallenges.ima.ulaval.ca.barchallenge;
 
 import android.content.Intent;
-import android.database.Cursor;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class SelfieChallengeActivity extends AppCompatActivity {
+public class SelfieChallengeActivity extends ActionBarActivity {
 
     private Button mTakePhotoBtn;
     private int TAKE_PHOTO_INTENT=999;
-    private int SELECT_IMAGE_INTENT=1234;
     private String mSelectedImagePath;
+    private Bitmap mPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +26,6 @@ public class SelfieChallengeActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
                 Intent cameraIntent = new Intent(
                         android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(cameraIntent, TAKE_PHOTO_INTENT);
@@ -38,24 +34,17 @@ public class SelfieChallengeActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        //Because there is a problem with orientation and camera
+    }
+
+    @Override
     protected void onActivityResult (int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==SELECT_IMAGE_INTENT && resultCode==RESULT_OK){
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
-            cursor.moveToFirst();
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-            mSelectedImagePath=picturePath;
-            ImageView imageView = (ImageView) findViewById(R.id.upload_image_preview);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-        }else if(requestCode==TAKE_PHOTO_INTENT && resultCode==RESULT_OK){
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            ImageView imageView = (ImageView) findViewById(R.id.upload_image_preview);
-            imageView.setImageBitmap(photo);
-            //we
+       if(requestCode==TAKE_PHOTO_INTENT && resultCode==RESULT_OK){
+           mPhoto = (Bitmap) data.getExtras().get("data");
+           ImageView imageView = (ImageView) findViewById(R.id.upload_image_preview);
+           imageView.setImageBitmap(mPhoto);
         }
     }
 }
